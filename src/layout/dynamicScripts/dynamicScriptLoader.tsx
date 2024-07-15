8429ee1d3be713c94824f7a9238aeffc045876d9
phenom_lavanya_ravinutala <lavanya.ravinutala@phenompeople.com>
2024-07-15 12:34:25 +0530

    changes

---
+++ src/layout/dynamicScripts/dynamicScriptLoader.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { AppStore } from "store";
+import { Loader } from "@phenom/react-ui-components";
+
+declare global {
+  interface Window {
+    txEmbed: any;
+  }
+}
+
+const ContentHub: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const storeData = useSelector((state: AppStore) => state.customer);
+  const selectedModuleAppObject = useSelector((state: any) => {
+    const selectedAppFromSession = JSON.parse(
+      sessionStorage.getItem("selectedApp") || "null"
+    );
+    return selectedAppFromSession || state.app?.selectedApp;
+  });
+    var selectedApp = selectedModuleAppObject?.appConfig || {};
+ 
+  useEffect(() => {
+    const embedScriptId = selectedApp?.scriptId;
+    const existsScrElem = document.querySelector(`#${embedScriptId}`);
+
+    const loadScript = () => {
+      return new Promise<void>((resolve) => {
+        if (!existsScrElem) {
+          const scrElem = document.createElement("script");
+          scrElem.id = embedScriptId;
+          scrElem.src =
+            selectedApp?.url
+          scrElem.onload = () => {
+            resolve();
+          };
+          document.querySelector("head")?.appendChild(scrElem);
+        } else {
+          resolve();
+        }
+      });
+    };
+
+    if (
+      storeData &&
+      storeData.selectedTenant &&
+      storeData.selectedTenant.refNum
+    ) {
+      loadScript().then(() => {
+        if (window.txEmbed) {
+          window.txEmbed.embedModules(selectedApp?.embedType, "#tools-body-container", {
+            refNum: storeData.selectedTenant.refNum,
+            token: window.keycloakInstance.token,
+          });
+        }
+        setIsLoading(false);
+      });
+    }
+  }, [storeData,selectedApp]);
+
+  return (
+    <div>
+      <div id="tools-body-container"></div>
+      {isLoading && (
+        <div>
+          <Loader title="Please Wait, Loading..." />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ContentHub;
