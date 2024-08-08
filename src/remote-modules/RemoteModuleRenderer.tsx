607dfdb1dd054157fb8e792062b1f4ab5f5e78ea
phenom_lavanya_ravinutala <lavanya.ravinutala@phenompeople.com>
2024-08-08 15:37:25 +0530

    rxjs-messageservice changes

--- src/remote-modules/RemoteModuleRenderer.tsx
+++ src/remote-modules/RemoteModuleRenderer.tsx
@@ -6,6 +6,9 @@ import { ReactAppRenderer } from "./ReactAppRenderer";
 import { AngularAppRenderer } from "./AngularAppRenderer";
 
 import { AppStore } from "store";
+import { appSelectionHandler, getAppByName } from "../utils/appUtils";
+import { setAppDetails } from "../store/apps/actions";
+import { MessageService } from "../MessageService";
 
 export const RemoteModuleRenderer = () => {
   const selectedTenant = useSelector(
@@ -14,8 +17,12 @@ export const RemoteModuleRenderer = () => {
 
   const { data, user, allTenants, customerTenants } = useSelector(
     (state: AppStore) => state.customer
-  );
+  );  
+  const customerDetails = useSelector((state: AppStore) => state.customer);
 
+  useEffect(()=>{
+    (window as any).TXEMessageService=MessageService;
+  },[])
   const APP_ENV = (window as any)._env_.APP_ENV;
   const selectedModuleAppObject = useSelector((state: any) => {
     const selectedAppFromSession = JSON.parse(
@@ -27,6 +34,93 @@ export const RemoteModuleRenderer = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [moduleProps, setModuleProps] = useState(selectedApp.props);
+
+  const fetchedApps = useSelector((state: any) => state.app.allApps);
+  const allEvents = fetchedApps.reduce(
+    (acc: any, app: any) => {
+      const { id, events } = app;
+      if (events?.inputs) {
+        const appInputs = events.inputs.map((input: any) => input);
+        if (!acc.inputs[id]) {
+          acc.inputs[id] = [];
+        }
+        acc.inputs[id].push(...appInputs);
+      }
+      if (events?.outputs) {
+        if (!acc.outputs[id]) {
+          acc.outputs[id] = [];
+        }
+        acc.outputs[id].push(...events.outputs);
+      }
+      return acc;
+    },
+    { inputs: {}, outputs: {} } // Changed outputs to an object
+  );
+  const emitEventsToChildApps = (output: any, currentEventData: any) => {
+    Object.keys(allEvents?.inputs).forEach((app) => {
+      const appInputs = allEvents.inputs[app];
+      appInputs.forEach((input: any) => {
+        if (input === output) {
+          console.log(
+            "Output matched with input, dispatching event...",
+            app + "_" + input,
+            currentEventData
+          );
+          MessageService.dispatchEvent(app + "_" + input, currentEventData);
+        }
+      });
+    });
+  };
+  const handleOutputs = (
+    output: any,
+    eventData: any,
+    currentEventData: any
+  ) => {
+    switch (output) {
+      case "NAVIGATE":
+        console.log("navigating to other app", eventData?.appName);
+        debugger
+        const navigatingApp = getAppByName(fetchedApps, eventData.appName);
+        navigatingApp &&
+          sessionStorage.setItem("selectedApp", JSON.stringify(navigatingApp));
+        dispatch(setAppDetails(navigatingApp));
+        appSelectionHandler(
+          navigatingApp,
+          navigate,
+          customerDetails?.customerCode,
+          selectedTenant?.refNum
+        );
+        break;
+      //add any other cases which has to be handled parent level
+      default:
+        console.log("Unhandled event at parent level:", eventData);
+        emitEventsToChildApps(output, currentEventData);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    const subscriptions = [] as any;
+    Object.keys(allEvents?.outputs)?.forEach((appId: string) => {
+      const appOutputs = allEvents?.outputs[appId];
+      appOutputs.forEach((output: any) => {
+        let currentEventData = {};
+        console.log("listening output", output);
+        const subscription = MessageService.on(appId + "_" + output).subscribe(
+          (eventData: any) => {
+            currentEventData = eventData;
+            handleOutputs(output, eventData, currentEventData);
+          }
+        );
+        subscriptions.push(subscription);
+      });
+    });
+
+    return () => {
+      // Unsubscribe all subscriptions when component is unmounted
+      subscriptions.forEach((subscription: any) => subscription.unsubscribe());
+    };
+  }, [fetchedApps]);
 
   useEffect(() => {
     setModuleProps((prevModuleProps: any) => {
