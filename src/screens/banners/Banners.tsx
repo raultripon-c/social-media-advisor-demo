cc2ebef36f7c5e2187c812d69637c6c35b60b1c9
abhishek_sha <abhishek.sharma@phenompeople.com>
2024-10-10 12:25:14 +0530

    Change for hiding banner cms loader

--- src/screens/banners/Banners.tsx
+++ src/screens/banners/Banners.tsx
@@ -4,62 +4,80 @@ import { AppStore } from "store";
 import { Loader } from "@phenom/react-ui-components";
 
 declare global {
-    interface Window {
-        txEmbed: any;
-    }
+  interface Window {
+    txEmbed: any;
+  }
 }
 const Banners = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const storeData = useSelector((state: AppStore) => state.customer);
-    const selectedModuleAppObject = useSelector((state: any) => {
-        const selectedAppFromSession = JSON.parse(
-          sessionStorage.getItem("selectedApp") || "null"
-        );
-        return selectedAppFromSession || state.app?.selectedApp;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const storeData = useSelector((state: AppStore) => state.customer);
+  const selectedModuleAppObject = useSelector((state: any) => {
+    const selectedAppFromSession = JSON.parse(sessionStorage.getItem("selectedApp") || "null");
+    return selectedAppFromSession || state.app?.selectedApp;
+  });
+  var selectedApp = selectedModuleAppObject?.appConfig || {};
+
+  // TODO: Will remove this in future
+  const deleteCmsLoader = () => {
+    const targetLoaderDiv = document.querySelector("#tools-body-container");
+    if (targetLoaderDiv) {
+      const loaderDiv: HTMLElement | null = targetLoaderDiv.querySelector(".ppc-loading");
+      if (loaderDiv) {
+        loaderDiv.style.display = "none";
+      }
+    }
+  };
+
+  useEffect(() => {
+    const embedScriptId = selectedApp?.scriptId;
+    const existsScrElem = document.querySelector(`#${embedScriptId}`);
+    if (existsScrElem) {
+      existsScrElem.remove();
+    }
+
+    const loadScript = () => {
+      return new Promise<void>((resolve) => {
+        if (!existsScrElem) {
+          const scrElem = document.createElement("script");
+          scrElem.id = embedScriptId;
+          scrElem.src = selectedApp?.url;
+          scrElem.onload = () => {
+            resolve();
+          };
+          document.querySelector("head")?.appendChild(scrElem);
+        } else {
+          resolve();
+        }
       });
-    var selectedApp = selectedModuleAppObject?.appConfig || {};
+    };
 
-    useEffect(() => {
-        const embedScriptId = selectedApp?.scriptId;
-        const existsScrElem = document.querySelector(`#${embedScriptId}`);
-        if(existsScrElem) {
-            existsScrElem.remove();
+    if (storeData && storeData.selectedTenant && storeData.selectedTenant.refNum) {
+      loadScript().then(() => {
+        if (window.txEmbed) {
+          window.txEmbed.embedModules("banners", "#tools-body-container", {
+            refNum: storeData.selectedTenant.refNum,
+            token: window.keycloakInstance.token,
+          });
         }
-
-        const loadScript = () => {
-            return new Promise<void>((resolve) => {
-                if (!existsScrElem) {
-                    const scrElem = document.createElement("script");
-                    scrElem.id = embedScriptId;
-                    scrElem.src = selectedApp?.url;
-                    scrElem.onload = () => {
-                        resolve();
-                    };
-                    document.querySelector("head")?.appendChild(scrElem);
-                } else {
-                    resolve();
-                }
-            });
-        };
-
-        if (storeData && storeData.selectedTenant && storeData.selectedTenant.refNum) { 
-            loadScript().then(() => {
-                if (window.txEmbed) {
-                    window.txEmbed.embedModules("banners", "#tools-body-container", {refNum: storeData.selectedTenant.refNum, token: window.keycloakInstance.token });
-                }
-                setIsLoading(false);
-            });
+        setIsLoading(false);
+        // Will remove this in future
+        for (let i = 1; i <= 3; i++) {
+            setTimeout(() => {
+              deleteCmsLoader();
+            }, 5000);
         }
-    }, [storeData]);
-    return (
+      });
+    }
+  }, [storeData]);
+  return (
+    <div>
+      <div id="tools-body-container"></div>
+      {isLoading && (
         <div>
-            <div id="tools-body-container"></div>
-            {isLoading && (
-                <div>
-                    <Loader title="Please Wait, Loading..." />
-                </div>
-            )}
+          <Loader title="Please Wait, Loading..." />
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 export default Banners;
