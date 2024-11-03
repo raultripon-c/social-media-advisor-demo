bec89353f52b62b6008e750eb250135c116672b0
Devarakonda Prakhyath <prakhyath.devarakonda@phenompeople.com>
2024-11-04 01:18:29 +0530

    handling routing based path

--- src/utils/appUtils.ts
+++ src/utils/appUtils.ts
@@ -336,9 +336,9 @@ export function findAppConfigByRoutes(apps: any = [], value: string): any {
   try {
     return apps.filter((element: any) => {
       try {
-        return value
-          .toLowerCase()
-          .includes(element?.appConfig?.route?.toLowerCase());
+        const currentAppPath = window.location.pathname.split('/').filter(Boolean)[2];
+        const configAppPath = element?.appConfig?.route.toLowerCase().split('/').filter(Boolean)[0];
+        return configAppPath === currentAppPath;
       } catch (error) {
         console.error("An error occurred while filtering: ", error);
         return false;
