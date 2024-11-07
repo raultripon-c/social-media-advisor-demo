ed5be9b588525cdca78f4807ca626faeb0a7b303
Devarakonda Prakhyath <prakhyath.devarakonda@phenompeople.com>
2024-11-07 08:52:03 +0530

    resolve promise overriding changes

--- src/layout/dynamicScripts/dynamicScriptLoader.tsx
+++ src/layout/dynamicScripts/dynamicScriptLoader.tsx
@@ -13,6 +13,7 @@ declare global {
 
 const ContentHub: React.FC = () => {
   removeCrmStyles();
+  (window as any).isCmsModule = true;
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const storeData = useSelector((state: AppStore) => state.customer);
   const selectedModuleAppObject = useSelector((state: any) => {
--- src/screens/banners/Banners.tsx
+++ src/screens/banners/Banners.tsx
@@ -12,6 +12,7 @@ declare global {
 }
 const Banners = () => {
   removeCrmStyles();
+  (window as any).isCmsModule = true;
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const storeData = useSelector((state: AppStore) => state.customer);
   const selectedModuleAppObject = useSelector((state: any) => {
